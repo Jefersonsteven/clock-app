508740ea77e3b51ea5ccca5eb8d853d3106b47a8
Jefersonsteven <gfreycode@gmail.com>
2023-07-07 14:51:13 -0500

    fetching of data completed

--- README.md
+++ README.md
@@ -2,33 +2,35 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+## The challenge
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+Your challenge is to build out this clock application and get it looking as close to the design as possible. You'll be using the following APIs to retrieve the necessary data:
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- [World Time API](http://worldtimeapi.org/) to set the time based on the visitor's IP adress. This API will also be used for additional data, like the day of the year shown in the expanded state.
+- [IP Geolocation API](https://freegeoip.app/) to set the city and country underneath the time
+- [Programming Quotes API](https://programming-quotes-api.herokuapp.com/) to generate random programming quotes.
+    - If the Programming Quotes API doesn't work, [here's a good alternative quote API](https://github.com/lukePeavey/quotable) you can use instead. It's not programming specific, but it will do the trick.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+Your users should be able to:
 
-## Learn More
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- View the current time and location information based on their IP address
+- View additional information about the date and time in the expanded state
+- Be shown the correct greeting and background image based on the time of day they're visiting the site
+- Generate random programming quotes by clicking the refresh icon near the quote
 
-To learn more about Next.js, take a look at the following resources:
+Want some support on the challenge? [Join our Slack community](https://www.frontendmentor.io/slack) and ask questions in the **#help** channel.
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+### Expected Behaviour
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- Change the greeting depending on the time of day. It should say:
+  - "Good morning" between 5am and 12pm
+  - "Good afternoon" between 12pm and 6pm
+  - "Good evening" between 6pm and 5am
+- Change the greeting icon and background image depending on the time of day. They should show:
+  - The sun icon and the daytime background image between 5am and 6pm
+  - The moon icon and the nighttime background image between 6pm and 5am
+- Generate a new random programming quote whenever the refresh icon is clicked
---
+++ src/app/page.jsx
@@ -0,0 +1,181 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+async function getData() {
+  const data = await fetch("http://worldtimeapi.org/api/ip");
+  const ip = await data.json();
+  const weather = await fetch(`http://worldtimeapi.org/api/ip/${ip.client_ip}`);
+  const ipdata = await fetch(
+    `http://ipinfo.io/${ip.client_ip}?token=4b8302d0ab73bd`
+  );
+  const ipinfo = await ipdata.json();
+  const weatherData = await weather.json();
+
+  return {
+    ipinfo,
+    weatherData,
+  };
+}
+
+async function getDataQuote() {
+  const quote = await fetch("https://api.quotable.io/random");
+  const quoteData = await quote.json();
+  return quoteData;
+}
+
+export default function Home() {
+  const [date, setDate] = useState();
+  const [weather, setWeather] = useState();
+  const [ipinfo, setIpInfo] = useState();
+  const [quote, setQuote] = useState();
+  const [open, setOpen] = useState(false);
+
+  async function getWeather() {
+    const {
+      weatherData,
+      ipinfo: { country, city },
+    } = await getData();
+    const date = new Date(weatherData.datetime);
+    setIpInfo({ country, city });
+    setWeather(weatherData);
+    setDate(date);
+  }
+
+  async function getQuote() {
+    const { content, author } = await getDataQuote();
+    setQuote({ content, author });
+  }
+
+  useEffect(() => {
+    getQuote();
+    getWeather();
+  }, []);
+
+  return (
+    <>
+      {quote && date && ipinfo && (
+        <main
+          className={` main text-white bg-cover h-full relative ${
+            date
+              ? date.getHours() < 18 &&
+                date.getHours() > 5 &&
+                `ss:bg-[url(/assets/mobile/bg-image-daytime.jpg)] sm:bg-[url(/assets/tablet/bg-image-daytime.jpg)] lg:bg-[url(/assets/desktop/bg-image-daytime.jpg)] `
+              : `ss:bg-[url(/assets/mobile/bg-image-nighttime.jpg)] sm:bg-[url(/assets/tablet/bg-image-nighttime.jpg)] lg:bg-[url(/assets/desktop/bg-image-nighttime.jpg)]`
+          }`}
+        >
+          <div className=" absolute w-full h-full bg-black z-10 opacity-30"></div>
+          <div className=" relative ss:p-8 h-full z-20">
+            <section className="h-full flex flex-col justify-between">
+              <div className="flex w-full justify-between gap-2">
+                <div className="flex flex-col gap-1 text-base">
+                  <p>{`"${quote.content}"`}</p>
+                  <h4 className=" font-bold">{quote.author}</h4>
+                </div>
+                <Image
+                  onClick={getQuote}
+                  src="/assets/desktop/icon-refresh.svg"
+                  alt="refresh"
+                  width={20}
+                  height={20}
+                  className=" w-6 h-6 cursor-pointer hover:opacity-50 transition-opacity duration"
+                />
+              </div>
+
+              <div>
+                <div>
+                  <div>
+                    {date.getHours() > 5 && date.getHours() < 18 ? (
+                      <Image
+                        src="/assets/desktop/icon-sun.svg"
+                        width={22}
+                        height={24}
+                        alt="sun"
+                      />
+                    ) : (
+                      <Image
+                        src="/assets/desktop/icon-moon.svg"
+                        width={22}
+                        height={24}
+                        alt="moon"
+                      />
+                    )}
+                    <p>
+                      {date.getHours() > 5 &&
+                        date.getHours() < 12 &&
+                        "GOOD MORNING"}
+                      {date.getHours() > 12 &&
+                        date.getHours() < 18 &&
+                        "GOOD AFTERNOON"}
+                      {date.getHours() > 18 &&
+                        date.getHours() < 5 &&
+                        "GOOD EVENING"}
+                    </p>
+                  </div>
+                  <div>
+                    <h1>
+                      {`${date.getHours()}:${`${
+                        date.getMinutes().toString().length === 1 ? "0" : ""
+                      }${date.getMinutes()}`}`}
+                    </h1>
+                    <p>BST</p>
+                  </div>
+                  <h2>
+                    {`
+                IN
+                ${ipinfo.city}
+                ,
+                ${ipinfo.country}
+              `}
+                  </h2>
+                </div>
+                <div>
+                  <div>
+                    <p>MORE</p>
+                    <div>
+                      {open === false ? (
+                        <Image
+                          src="/assets/desktop/icon-arrow-down.svg"
+                          alt="arrow"
+                          width={10}
+                          height={5}
+                        />
+                      ) : (
+                        <Image
+                          src="/assets/desktop/icon-arrow-up.svg"
+                          alt="arrow"
+                          width={10}
+                          height={5}
+                        />
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            <section className="absolute hidden">
+              <ul>
+                <li>
+                  <h3>CURRENT TIMEZONE</h3> <p>{weather && weather.timezone}</p>
+                </li>
+                <li>
+                  <h3>DAY OF THE YEAR</h3>
+                  <p>{weather && weather.day_of_year}</p>
+                </li>
+                <li>
+                  <h3>DAY OF THE WEEK</h3>
+                  <p>{weather && weather.day_of_week}</p>
+                </li>
+                <li>
+                  <h3>WEEK NUMBER</h3> <p>{weather && weather.week_number}</p>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </main>
+      )}
+    </>
+  );
+}

2df9b85b26b9d49219dbd1b86dfef28ca0a788ec
Jefersonsteven <gfreycode@gmail.com>
2023-07-08 00:14:10 -0500

    done mobile tablet an desktop

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -58,34 +58,40 @@ export default function Home() {
       {quote && date && ipinfo && (
         <main
           className={` main text-white bg-cover h-full relative ${
-            date
-              ? date.getHours() < 18 &&
-                date.getHours() > 5 &&
-                `ss:bg-[url(/assets/mobile/bg-image-daytime.jpg)] sm:bg-[url(/assets/tablet/bg-image-daytime.jpg)] lg:bg-[url(/assets/desktop/bg-image-daytime.jpg)] `
-              : `ss:bg-[url(/assets/mobile/bg-image-nighttime.jpg)] sm:bg-[url(/assets/tablet/bg-image-nighttime.jpg)] lg:bg-[url(/assets/desktop/bg-image-nighttime.jpg)]`
+            date.getHours() < 18 && date.getHours() > 5
+              ? `ss:bg-[url(/assets/mobile/bg-image-daytime.jpg)] sm:bg-[url(/assets/tablet/bg-image-daytime.jpg)] xl:bg-[url(/assets/desktop/bg-image-daytime.jpg)] `
+              : `ss:bg-[url(/assets/mobile/bg-image-nighttime.jpg)] sm:bg-[url(/assets/tablet/bg-image-nighttime.jpg)] xl:bg-[url(/assets/desktop/bg-image-nighttime.jpg)]`
           }`}
         >
-          <div className=" absolute w-full h-full bg-black z-10 opacity-30"></div>
-          <div className=" relative ss:px-8 ss:py-16 h-full z-20">
-            <section className="h-full flex flex-col justify-between">
-              <div className="flex w-full justify-between gap-2">
-                <div className="flex flex-col gap-1 text-[15px] tracking-[3px]">
+          <div className=" absolute w-full h-full bg-black z-10 ss:opacity-30 sm:opacity-40"></div>
+          <div className=" relative h-full z-20 flex flex-col justify-between">
+            <section
+              className={`h-full flex flex-col ${
+                open ? "justify-end" : "justify-between"
+              }  ss:px-8 ss:py-16 sm:px-24 sm:py-32 xl:px-60 xl:py-48`}
+            >
+              <div
+                className={`flex ss:w-full sm:w-10/12 xl:w-2/4 justify-between ss:gap-2 sm:gap-3 ${
+                  open && "hidden"
+                }`}
+              >
+                <article className="flex flex-col ss:gap-1 sm:gap-3 ss:text-[15px] sm:text-[18px] tracking-[3px]">
                   <p>{`"${quote.content}"`}</p>
-                  <h4 className=" font-bold">{quote.author}</h4>
-                </div>
+                  <cite className=" font-bold">{quote.author}</cite>
+                </article>
                 <Image
                   onClick={getQuote}
                   src="/assets/desktop/icon-refresh.svg"
                   alt="refresh"
                   width={20}
                   height={20}
-                  className=" w-6 h-6 cursor-pointer hover:opacity-50 transition-opacity duration"
+                  className=" ss:w-6 ss:h-6 sm:w-7 sm:h-7 cursor-pointer hover:opacity-50 transition-opacity duration"
                 />
               </div>
 
-              <div className="flex flex-col gap-[54px]">
-                <div className="flex flex-col gap-10">
-                  <div className=" flex gap-8">
+              <div className="flex flex-col ss:gap-[54px] sm:gap-[76px] xl:flex-row  xl:items-end xl:justify-between">
+                <div className="flex flex-col ss:gap-10 sm:gap-20 xl:gap-30">
+                  <div className=" flex gap-8 sm:text-[18px] xl:text-[20px]">
                     {date.getHours() > 5 && date.getHours() < 18 ? (
                       <Image
                         src="/assets/desktop/icon-sun.svg"
@@ -99,43 +105,46 @@ export default function Home() {
                         width={22}
                         height={24}
                         alt="moon"
+                        className=" xl:w-[24px] xl:h-[26px]"
                       />
                     )}
-                    <p className=" tracking-[4px]">
-                      {date.getHours() > 5 &&
-                        date.getHours() < 12 &&
-                        "GOOD MORNING"}
-                      {date.getHours() > 12 &&
-                        date.getHours() < 18 &&
-                        "GOOD AFTERNOON"}
-                      {date.getHours() > 18 &&
-                        date.getHours() < 5 &&
-                        "GOOD EVENING"}
-                    </p>
+                    <div className=" tracking-[4px] flex">
+                      <p>
+                        {date.getHours() > 5 &&
+                          date.getHours() <= 12 &&
+                          "GOOD MORNING"}
+                        {date.getHours() > 12 &&
+                          date.getHours() < 18 &&
+                          "GOOD AFTERNOON"}
+                        {(date.getHours() >= 18 || date.getHours() < 5) &&
+                          "GOOD EVENING"}
+                      </p>
+                      <p className=" ss:hidden sm:inline">, IT’S CURRENTLY</p>
+                    </div>
                   </div>
                   <div className="flex gap-3 items-end h-auto">
-                    <h1 className=" ss:text-[100px] font-extrabold leading-[80px]">
-                      {`${date.getHours()}:${`${
-                        date.getMinutes().toString().length === 1 ? "0" : ""
+                    <h1 className=" ss:text-[100px] sm:text-[175px] xl:text-[200px] font-extrabold leading-[80px] xl:leading-[200px]">
+                      {`${
+                        date.getHours() < 10 ? "0" : ""
+                      }${date.getHours()}:${`${
+                        date.getMinutes() < 10 ? "0" : ""
                       }${date.getMinutes()}`}`}
                     </h1>
-                    <p>BST</p>
+                    <p className="xl:text-[40px]">BST</p>
                   </div>
-                  <h2>
-                    {`
-                IN
-                ${ipinfo.city}
-                ,
-                ${ipinfo.country}
-              `}
+                  <h2 className=" uppercase font-bold ss:tracking-widest sm:tracking-[3px] sm:text-[18px] xl:text-[24px]">
+                    {`IN ${ipinfo.city}, ${ipinfo.country}`}
                   </h2>
                 </div>
-                <div className=" rounded-full bg-white text-[#303030] w-[100px]">
-                  <div className="flex gap-2 p-2 pl-6 justify-between">
-                    <p className=" font-bold tracking-widest text-[12px] leading-[26px]">
+                <div className=" rounded-full bg-white text-[#303030] ss:w-[100px] sm:w-[120px] xl:h-[56px] xl:w-[156px] xl:flex xl:">
+                  <div className="flex gap-2 p-2 pl-6 justify-between xl:w-full xl:items-center xl:pl-8">
+                    <p className=" font-bold tracking-widest ss:text-[12px] sm:text-[15px] ss:leading-[26px] sm:leading-[30px] xl:leading-[45px]">
                       {open === false ? "MORE" : "LESS"}
                     </p>
-                    <div className=" w-12 h-12 bg-[#303030] rounded-full grid place-items-center cursor-pointer">
+                    <div
+                      onClick={() => setOpen((preventOpen) => !preventOpen)}
+                      className=" ss:w-12 ss:h-12 sm:w-14 sm:h-14 bg-[#303030] rounded-full grid place-items-center cursor-pointer xl:w-16 xl:h-16"
+                    >
                       {open === false ? (
                         <Image
                           src="/assets/desktop/icon-arrow-down.svg"
@@ -157,21 +166,47 @@ export default function Home() {
               </div>
             </section>
 
-            <section className="absolute hidden">
-              <ul>
-                <li>
-                  <h3>CURRENT TIMEZONE</h3> <p>{weather && weather.timezone}</p>
+            <section
+              className={`ss:px-10 ss:py-16 backdrop-blur-xl sm:py-32 ${
+                !open && "hidden"
+              } ${
+                date.getHours() > 5 && date.getHours() < 18
+                  ? "bg-[#ffffff87]"
+                  : "bg-[#0000008c]"
+              }`}
+            >
+              <ul className="flex ss:flex-col ss:gap-4 sm:flex-wrap sm:justify-center sm:items-center sm:gap-12 sm:max-h-[260px]">
+                <li className=" flex w-full justify-between items-center sm:flex-col sm:w-[40%] sm:items-start">
+                  <h3 className=" ss:text-base tracking-[4px] sm:text-[16px]">
+                    CURRENT TIMEZONE
+                  </h3>{" "}
+                  <p className=" font-bold ss:text-[24px] sm:text-[40px]">
+                    {weather && weather.timezone}
+                  </p>
                 </li>
-                <li>
-                  <h3>DAY OF THE YEAR</h3>
-                  <p>{weather && weather.day_of_year}</p>
+                <li className=" flex w-full justify-between items-center sm:flex-col sm:w-[40%] sm:items-start">
+                  <h3 className=" text-base tracking-[4px] sm:text-[16px]">
+                    DAY OF THE YEAR
+                  </h3>
+                  <p className=" font-bold ss:text-[24px] sm:text-[40px]">
+                    {weather && weather.day_of_year}
+                  </p>
                 </li>
-                <li>
-                  <h3>DAY OF THE WEEK</h3>
-                  <p>{weather && weather.day_of_week}</p>
+                <li className=" flex w-full justify-between items-center sm:flex-col sm:w-[40%] sm:items-start">
+                  <h3 className=" text-base tracking-[4px] sm:text-[16px]">
+                    DAY OF THE WEEK
+                  </h3>
+                  <p className=" font-bold ss:text-[24px] sm:text-[40px]">
+                    {weather && weather.day_of_week}
+                  </p>
                 </li>
-                <li>
-                  <h3>WEEK NUMBER</h3> <p>{weather && weather.week_number}</p>
+                <li className=" flex w-full justify-between items-center sm:flex-col sm:w-[40%] sm:items-start">
+                  <h3 className=" text-base tracking-[4px] sm:text-[16px]">
+                    WEEK NUMBER
+                  </h3>{" "}
+                  <p className=" font-bold ss:text-[24px] sm:text-[40px]">
+                    {weather && weather.week_number}
+                  </p>
                 </li>
               </ul>
             </section>

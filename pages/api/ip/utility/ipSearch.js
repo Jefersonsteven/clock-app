a1519888d1b3325683d5b782dbed953367eec46c
Jefersonsteven <gfreycode@gmail.com>
2023-07-10 06:48:06 -0500

    refactor: replace api per librarie

--- pages/api/ip/utility/ipSearch.js
+++ pages/api/ip/utility/ipSearch.js
@@ -1,17 +1,31 @@
+import moment from "moment";
+
 async function getData() {
   const data = await fetch("https://ipapi.co/json/");
-
   const ip = await data.json();
-  const weather = await fetch(`https://worldtimeapi.org/api/ip/${ip.ip}`);
   const ipdata = await fetch(
     `https://ipinfo.io/${ip.ip}?token=${process.env.API_KEY}`
   );
   const ipinfo = await ipdata.json();
-  const weatherData = await weather.json();
+  const response = await fetch(
+    `https://api.ipgeolocation.io/timezone?apiKey=${process.env.API_KEY_TIME}&tz=${ipinfo.timezone}`
+  );
+  const datetime = await response.json();
+
+  const date = moment(datetime.date_time_ymd);
+  const day_of_week = date.day();
+  const day_of_year = date.dayOfYear();
+  const week_number = date.week();
 
   return {
     ipinfo,
-    weatherData,
+    weatherData: {
+      day_of_week,
+      day_of_year,
+      week_number,
+      timezone: datetime.timezone,
+      datetime: datetime.date_time_ymd,
+    },
   };
 }
 
